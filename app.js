63b88077b9b59c9b963a6593680a7815019504af
julie <julie.minjung.chung@gmail.com>
2015-10-10 17:27:50 -0400

    fixed spelling

--- app.js
+++ app.js
@@ -3,30 +3,30 @@ angular.module("interestsApp",["firebase"])
     $scope.interests = [{
       name:"Coding",
       numberOfPeople:42,
-      Description:"If your interested in coding in anyway, these are the people for you.",
+      Description:"If you are interested in coding in anyway, these are the people for you.",
       link:""
     },{
       name:"Basketball",
       numberOfPeople:23,
-      Description:"If your interested in hooping, these are the people for you.",
+      Description:"If you are interested in hooping, these are the people for you.",
       link:"https://docs.angularjs.org/api/ng/directive/ngHref"
     },
     {
         name:"Video Games",
       numberOfPeople:55,
-      Description:"If your interested in Video Games, these are the people for you.",
+      Description:"If you are interested in Video Games, these are the people for you.",
       link:""
     },
     {
       name:"Fashion",
       numberOfPeople:23,
-      Description:"If your interested in fashion, these are the people for you.",
+      Description:"If you are interested in fashion, these are the people for you.",
       link:""
     },
     {
       name:"Fishing",
       numberOfPeople:23,
-      Description:"If your interested in Fishing, these are the people for you.",
+      Description:"If you are interested in Fishing, these are the people for you.",
       link:""
     }];
 });
