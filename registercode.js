89ef4b7a7ed590b3506a167cadbc165832288eba
julie <julie.minjung.chung@gmail.com>
2015-10-10 14:41:00 -0400

    faked the register and login page

--- registercode.js
+++ registercode.js
@@ -12,3 +12,14 @@ user.signUp(null, {
     alert("Error: " + error.code + " " + error.message);
   }
 }); **/
+
+   function store(theForm) {
+    document.getElementById('welcomeMessage').innerHTML = "";
+    var inputUsername= theForm["username"];
+    var inputPassword= theForm["password"];
+    localStorage.setItem("username", inputUsername.value);
+    localStorage.setItem("password", inputPassword.value);
+    document.getElementById('welcomeMessage').innerHTML = "Welcome " + localStorage.getItem('username') + "!";
+    return false;
+   } // end store()
+//function to sign in
