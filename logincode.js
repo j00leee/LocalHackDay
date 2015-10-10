f16af81f8725f453765b234e3f8fa72b9153682c
julie <julie.minjung.chung@gmail.com>
2015-10-10 14:19:42 -0400

    haha

---
+++ logincode.js
@@ -0,0 +1,13 @@
+/**
+
+if(userid === "jchung" && pswd === "12345"){
+	.alert("Continue on");
+}
+
+}
+else{
+	.alert("Wrong username or password!");
+	$("#Login").prop("disabled",true);
+}
+
+**/
---
+++ registercode.js
@@ -0,0 +1,14 @@
+/**var user = new Parse.User();
+user.set("username", "my name");
+user.set("password", "my pass");
+
+
+user.signUp(null, {
+  success: function(user) {
+    // Hooray! Let them use the app now.
+  },
+  error: function(user, error) {
+    // Show the error message somewhere and let the user try again.
+    alert("Error: " + error.code + " " + error.message);
+  }
+}); **/
