353f66a0a74cb5575dbebd1f3f7b3b2f32a0a529
Subin <subjsam@gmail.com>
2015-10-10 20:09:40 -0400

    changed descriptions

--- interests.js
+++ interests.js
@@ -9,10 +9,10 @@ angular.module("interestsApp",[])
       name:"Basketball",
       numberOfPeople:23,
       Description:"If Ball is Life, these are the people for you.",
-      link:"https://docs.angularjs.org/api/ng/directive/ngHref"
+      link:""
     },
     {
-      name:"Video Games",
+      name:"Video games",
       numberOfPeople:55,
       Description:"If your interesting in beating noobs, these are the people for you.",
       link:""
